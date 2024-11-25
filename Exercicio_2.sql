43333cc2bcad81b3e828ab43f250f2ed3265d5e5
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-11-24 21:54:29 -0300

    Exercicio 2 Feito.

---
+++ Exercicio_2.sql
@@ -0,0 +1,21 @@
+create database if not exists loja_virtual;
+use loja_virtual;
+
+create table if not exists produtos(
+				id int primary key auto_increment,
+				nome varchar(255),
+                qtd_estq int,
+                valor decimal,
+                marca varchar(50)
+    );
+    
+insert into produtos values (null, "Vinho", 200, 750.00, "Colorado");
+insert into produtos values (null, "Arroz", 200, 10.00, "Camil");
+insert into produtos values (null, "Feijão", 150, 3.00, "Camil");
+insert into produtos values (null, "Cerveja", 220, 4.50, "Heinekein");
+insert into produtos values (null, "Vodka", 300, 7.00, "Heinekein");
+select * from produtos;
+select * from produtos where valor > 500;
+select * from produtos where valor < 500;
+
+update produtos set valor = 800.00 where id = 1;
